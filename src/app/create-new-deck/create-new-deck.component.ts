5b39c541f9c8394cbc1fe025a095ea297d1f58d6
carter <speers.carter@gmail.com>
2024-02-27 20:11:46 -0500

    added create new decK screen

---
+++ src/app/create-new-deck/create-new-deck.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-new-deck',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-new-deck.component.html',
+  styleUrl: './create-new-deck.component.css'
+})
+export class CreateNewDeckComponent {
+
+}
